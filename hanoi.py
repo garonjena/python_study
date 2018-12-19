9f6462cd20f4c0f2b03891b32651d02d60b70e9a
garonjena <36976959+garonjena@users.noreply.github.com>
2018-12-19 17:38:27 +0900

    Update hanoi.py

--- hanoi.py
+++ hanoi.py
@@ -10,7 +10,7 @@ def hanoi(num_discs, start_peg, end_peg):
         print("%d번 원판을 %d번 기둥에서 %d번 기둥으로 이동" % (num_discs, start_peg, end_peg))
         hanoi(num_discs - 1, other_peg, end_peg)
 
-# 테스트 코드 (포함하여 제출해주세요)
+# 테스트 코드
 hanoi(3, 1, 3)
 
 # 가장 큰 원판을 제외하고 나머지 원판들을 start_peg에서 other_peg로 이동
